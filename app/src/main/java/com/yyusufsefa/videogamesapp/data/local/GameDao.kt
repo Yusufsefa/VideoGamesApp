7ae2eb86531c10f8ee01eb165c306c70fd803f71
Yusuf Sefa ALİŞ <yusufsefa61@gmail.com>
2021-04-16 17:24:37 +0300

    fix dao

--- app/src/main/java/com/yyusufsefa/videogamesapp/data/local/GameDao.kt
+++ app/src/main/java/com/yyusufsefa/videogamesapp/data/local/GameDao.kt
@@ -13,7 +13,7 @@ interface GameDao {
     @Query("SELECT * FROM video_game")
     fun getAllGame(): LiveData<List<Game>>
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(games: List<Game>)
 
     @Query("DELETE FROM video_game WHERE _id= :gameId")
@@ -24,4 +24,10 @@ interface GameDao {
 
     @Query("UPDATE video_game SET isLiked = :isLiked WHERE _id = :id")
     suspend fun update(isLiked: Boolean, id: Int)
+
+    @Query("SELECT * FROM video_game WHERE _id = :id ")
+    fun getGame(id: Int): LiveData<Game>
+
+    @Query("SELECT * FROM video_game WHERE isLiked = 1")
+    fun getLikedGame(): LiveData<List<Game>>
 }
